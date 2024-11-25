c1aee62ba0a2b94801f13b2664a1b63fd09948e0
tjyu1999 <tjyu1999@gmail.com>
2024-11-25 22:59:57 +0800

    add sort/sort.h

---
+++ sort/sort.h
@@ -0,0 +1,13 @@
+#ifndef _SORT
+#define _SORT
+
+#include <iostream>
+#include <vector>
+
+void insertSort(std::vector<int> &);
+void bubbleSort(std::vector<int> &);
+void quickSort(std::vector<int> &);
+void mergeSort(std::vector<int> &);
+void heapSort(vector<int> &);
+
+#endif
