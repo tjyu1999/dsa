db44b77021b6731238f4902579296c99c9084e82
tjyu1999 <tjyu1999@gmail.com>
2024-11-26 21:39:04 +0800

    add stack/demo.cpp

---
+++ stack/demo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdexcept>
+#include "stack.h"
+
+int main() {
+    try {
+        Stack st(3);
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        std::cout << "st.top() = " << st.top() << std::endl;
+        st.pop();
+        std::cout << "st.pop() = " << st.top() << std::endl;
+        st.push(40);
+        std::cout << "st.top() = " << st.top() << std::endl;
+        st.pop();
+        st.pop();
+        st.pop();
+        std::cout << "st.empty() = " << st.empty() << std::endl;
+        std::cout << "st.size() = " << st.size() << std::endl;
+        st.pop();
+    }
+    catch(const std::runtime_error &e) {
+        std::cerr << "Stack Exception: " << e.what() << std::endl;
+    }
+}
