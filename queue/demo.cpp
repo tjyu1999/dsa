495560a84e4291537c6b083fb515c6e21ca69d3c
tjyu1999 <tjyu1999@gmail.com>
2024-11-27 15:42:40 +0800

    edit queue/demo.cpp

--- queue/demo.cpp
+++ queue/demo.cpp
@@ -5,25 +5,25 @@
 int main() {    
     try {
         Queue q(3);
-        q.push(10);
-        q.push(20);
-        q.push(30);
-        std::cout << "\nst.front() = " << q.front() << std::endl;
-        std::cout << "st.back() = " << q.back() << std::endl;
+        q.push(1);
+        q.push(2);
+        q.push(3);
+        std::cout << "q.front() = " << q.front() << std::endl;
+        std::cout << "q.back() = " << q.back() << std::endl;
         q.pop();
-        std::cout << "st.front() = " << q.front() << std::endl;
-        q.push(40);
-        std::cout << "st.back() = " << q.back() << std::endl;
+        std::cout << "q.front() = " << q.front() << std::endl;
+        q.push(4);
+        std::cout << "q.back() = " << q.back() << std::endl;
         q.pop();
         q.pop();
         q.pop();
-        std::cout << "st.empty() = " << q.empty() << std::endl;
-        std::cout << "st.size() = " << q.size() << std::endl;
+        std::cout << "q.empty() = " << q.empty() << std::endl;
+        std::cout << "q.size() = " << q.size() << std::endl;
         q.pop();
     }
     catch(const std::runtime_error &e) {
         std::cerr << "Queue Exception: " << e.what() << std::endl;
-    }
-        
+    }    
+    
     return 0;
 }
