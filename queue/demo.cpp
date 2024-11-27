e261684dc3431e7613a84877586e0fe83c97ba31
tjyu1999 <tjyu1999@gmail.com>
2024-11-27 23:09:02 +0800

    edit queue/demo.cpp

--- queue/demo.cpp
+++ queue/demo.cpp
@@ -9,16 +9,16 @@ int main() {
         q.push(2);
         q.push(3);
         std::cout << "q.front() = " << q.front() << std::endl;
-        std::cout << "q.back() = " << q.back() << std::endl;
+        std::cout << "q.back()  = " << q.back() << std::endl;
         q.pop();
         std::cout << "q.front() = " << q.front() << std::endl;
         q.push(4);
-        std::cout << "q.back() = " << q.back() << std::endl;
+        std::cout << "q.back()  = " << q.back() << std::endl;
         q.pop();
         q.pop();
         q.pop();
         std::cout << "q.empty() = " << q.empty() << std::endl;
-        std::cout << "q.size() = " << q.size() << std::endl;
+        std::cout << "q.size()  = " << q.size() << std::endl;
         q.pop();
     }
     catch (const std::runtime_error &e) {
