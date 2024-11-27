eb978123a08f932b318b9837828de6eba66869f7
tjyu1999 <tjyu1999@gmail.com>
2024-11-27 23:09:30 +0800

    edit stack/demo.cpp

--- stack/demo.cpp
+++ stack/demo.cpp
@@ -8,16 +8,16 @@ int main() {
         st.push(10);
         st.push(20);
         st.push(30);
-        std::cout << "st.top() = " << st.top() << std::endl;
+        std::cout << "st.top()  = " << st.top() << std::endl;
         st.pop();
-        std::cout << "st.pop() = " << st.top() << std::endl;
+        std::cout << "st.pop()  = " << st.top() << std::endl;
         st.push(40);
-        std::cout << "st.top() = " << st.top() << std::endl;
+        std::cout << "st.top()  = " << st.top() << std::endl;
         st.pop();
         st.pop();
         st.pop();
         std::cout << "st.empty() = " << st.empty() << std::endl;
-        std::cout << "st.size() = " << st.size() << std::endl;
+        std::cout << "st.size()  = " << st.size() << std::endl;
         st.pop();
     }
     catch (const std::runtime_error &e) {
