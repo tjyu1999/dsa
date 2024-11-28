c6a4ecd89e23b80948d4ce7fac74b74dfc90a3f4
tjyu1999 <tjyu1999@gmail.com>
2024-11-28 20:34:24 +0800

    edit sort/sort.h

--- sort/sort.h
+++ sort/sort.h
@@ -1,13 +1,22 @@
 #ifndef _SORT
 #define _SORT
 
-#include <iostream>
 #include <vector>
 
-void insertSort(std::vector<int> &);
+void insertionSort(std::vector<int> &);
+
 void bubbleSort(std::vector<int> &);
+
+int partition(std::vector<int> &, int, int);
+void quickSort(std::vector<int> &, int, int);
 void quickSort(std::vector<int> &);
+
+void merge(std::vector<int> &, int, int, int);
+void mergeSort(std::vector<int> &, int, int);
 void mergeSort(std::vector<int> &);
-void heapSort(vector<int> &);
+
+void heapSort(std::vector<int> &);
+void swapHeap(std::vector<int> &, int, int);
+void downHeap(std::vector<int> &, int, int);
 
 #endif
