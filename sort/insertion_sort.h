cd1e548d9861d498eb6eded38ddb809e125059f0
tjyu1999 <tjyu1999@gmail.com>
2026-06-08 23:06:04 +0800

    feat: add insertion sort implementation

---
+++ sort/insertion_sort.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+
+namespace sort {
+    template <typename T>
+    void insertion_sort(std::vector<T> &arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            T temp = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && temp < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = temp;
+        }
+    }
+}
