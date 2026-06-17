6c20e5a02d7a169fc65f1276ec645e5a13920e07
tjyu1999 <tjyu1999@gmail.com>
2026-06-17 22:45:56 +0800

    refactor: rename quick_sort to quick_sort_internal

--- src/sort/quick_sort.hpp
+++ src/sort/quick_sort.hpp
@@ -1,15 +1,15 @@
 #pragma once
+#include <algorithm>
 #include <vector>
 
 
 namespace sort {
     template <typename T>
-    int partition(std::vector<T> &arr, const int low, const int high) {
-        T pivot = arr[high];
+    int partition(std::vector<T> &arr, int low, int high) {
+        const T& pivot = arr[high];
         int i = low - 1;
-        int j;
 
-        for (j = low; j < high; j++) {
+        for (int j = low; j < high; j++) {
             if (arr[j] <= pivot)
                 std::swap(arr[++i], arr[j]);
         }
@@ -19,18 +19,18 @@ namespace sort {
     }
 
     template <typename T>
-    void quick_sort(std::vector<T> &arr, int low, int high) {
+    void quick_sort_internal(std::vector<T> &arr, int low, int high) {
         if (low < high) {
             int p = partition(arr, low, high);
 
-            quick_sort(arr, low, p - 1);
-            quick_sort(arr, p + 1, high);
+            quick_sort_internal(arr, low, p - 1);
+            quick_sort_internal(arr, p + 1, high);
         }
     }
 
     template <typename T>
     void quick_sort(std::vector<T> &arr) {
         if (!arr.empty())
-            quick_sort(arr, 0, (int)arr.size() - 1);
+            quick_sort_internal(arr, 0, (int)arr.size() - 1);
     }
 }
